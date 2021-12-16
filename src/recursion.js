203a3f93a2f3ba91119716d862b0572dcab94049
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-12-16 14:04:44 +0100

    fourth

--- src/recursion.js
+++ src/recursion.js
@@ -37,7 +37,17 @@ var arraySum = function (array) {
 };
 
 // 4. Check if a number is even.
-var isEven = function (n) {};
+var isEven = function (n) {
+  if (n === 0) {
+    return true;
+  } else if (n === 1) {
+    return false;
+  } else if (n > 0) {
+    return isEven(n - 2);
+  } else {
+    return isEven(-n);
+  }
+};
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45

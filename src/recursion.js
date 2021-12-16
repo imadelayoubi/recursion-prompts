f92e8081b4edea1087ff7eb1cb94b3022ab380b6
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-12-16 13:12:48 +0100

    third

--- src/recursion.js
+++ src/recursion.js
@@ -24,7 +24,17 @@ var sum = function (array) {
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
-var arraySum = function (array) {};
+var arraySum = function (array) {
+  var sum = 0;
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      sum += arraySum(item);
+    } else {
+      sum += item;
+    }
+  });
+  return sum;
+};
 
 // 4. Check if a number is even.
 var isEven = function (n) {};

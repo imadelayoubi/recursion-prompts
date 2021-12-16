148259a392492bce269433881bb0dbba5610d92a
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-12-16 14:11:07 +0100

    fth

--- src/recursion.js
+++ src/recursion.js
@@ -52,7 +52,14 @@ var isEven = function (n) {
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
-var sumBelow = function (n) {};
+var sumBelow = function (n) {
+  var result = 0;
+  if (n === 1) {
+    return 1;
+  }
+  result += sumBelow(n - 1);
+  return result;
+};
 
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
